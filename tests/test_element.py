2c7b916ed9a4d11adb10858fd6535f40a76eb87d
Scott Russell <me@scottrussell.net>
2020-11-14 01:00:47 -0600

    add test

--- tests/test_element.py
+++ tests/test_element.py
@@ -1,5 +1,16 @@
-
+from hype import *
 
 
 def test_element():
-    assert True
+
+    class Test(Element):
+        tag='test'
+
+    test = Test(4)
+    assert str(test) == '\ntest'
+
+
+def test_div():
+    div = Div(4)
+    assert str(div) == '\n<div>4</div>'
+
