fb1f04049ad304569e6613e729942cfe15b699eb
Scott Russell <me@scottrussell.net>
2020-11-14 00:17:35 -0600

    initial commit

---
+++ scripts/add_attribute.py
@@ -0,0 +1,20 @@
+import sys
+import json
+
+
+def add_attribute(attr, *matched_tags):
+    with open('tags.json', 'r') as f:
+        tags = json.loads(f.read())
+
+    for tag, data in tags.items():
+        if tag in matched_tags:
+            data.get('attrs').append(attr)
+        if len(matched_tags) == 1 and matched_tags[0] == 'global':
+            data.get('attrs').append(attr)
+
+    with open('tags.json', 'w') as f:
+        f.write(json.dumps(tags, indent=2))
+
+
+if __name__ == '__main__':
+    add_attribute(sys.argv[1], *sys.argv[2:])
---
+++ scripts/pre_process.py
@@ -0,0 +1,26 @@
+import json
+
+from jinja2 import Template
+
+
+def generate():
+    with open('tags.json', 'r') as f:
+        tags = json.loads(f.read())
+
+    with open('templates/element.py.j2', 'r') as f:
+        template = f.read()
+    tmp = Template(template)
+    code = tmp.render(tags=tags)
+    with open('element.py', 'w') as f:
+        f.write(code)
+
+    with open('templates/__init__.py.j2', 'r') as f:
+        template = f.read()
+    tmp = Template(template)
+    code = tmp.render(tags=tags)
+    with open('__init__.py', 'w') as f:
+        f.write(code)
+
+
+if __name__ == '__main__':
+    generate()
